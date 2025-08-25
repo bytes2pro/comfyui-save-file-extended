a8a3bf17a4656952980e7a075c15f7e5e5be31fd
RUiNtheExtinct <deepkarma001@gmail.com>
2025-08-25 22:33:57 +0530

    Initial commit

---
+++ __init__.py
@@ -0,0 +1,16 @@
+"""Top-level package for comfyui_save_file_extended."""
+
+__all__ = [
+    "NODE_CLASS_MAPPINGS",
+    "NODE_DISPLAY_NAME_MAPPINGS",
+    
+]
+
+__author__ = """RUiNtheExtinct"""
+__email__ = "deepkarma001@gmail.com"
+__version__ = "0.0.1"
+
+from .src.comfyui_save_file_extended.nodes import NODE_CLASS_MAPPINGS
+from .src.comfyui_save_file_extended.nodes import NODE_DISPLAY_NAME_MAPPINGS
+
+
