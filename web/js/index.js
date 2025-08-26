deda77809e8485f115ef893c803682d0e2187468
RUiNtheExtinct <deepkarma001@gmail.com>
2025-08-26 23:00:43 +0530

    frontend working now

--- web/js/index.js
+++ web/js/index.js
@@ -1,32 +1,14 @@
 import { setupProgress } from "./progress.js";
 import { setupToasts } from "./toasts.js";
 import { beforeRegisterNodeDef as uiBeforeRegisterNodeDef } from "./ui_enhance.js";
+import { app } from "/scripts/app.js";
 
-(function registerOnceWhenAppReady() {
-    if (window.__CSE_REGISTERED__) return;
-
-    const tryRegister = () => {
-        const app = window.app;
-        if (!app) return false;
-        if (window.__CSE_REGISTERED__) return true;
-
-        app.registerExtension({
-            name: "comfyui.savefileextended",
-            async setup(app) {
-                await Promise.all([setupProgress(app), setupToasts(app)]);
-            },
-            async beforeRegisterNodeDef(nodeType, nodeData, app) {
-                await uiBeforeRegisterNodeDef(nodeType, nodeData, app);
-            },
-        });
-
-        window.__CSE_REGISTERED__ = true;
-        return true;
-    };
-
-    if (!tryRegister()) {
-        const interval = setInterval(() => {
-            if (tryRegister()) clearInterval(interval);
-        }, 50);
-    }
-})();
+app.registerExtension({
+    name: "comfyui.savefileextended",
+    async setup(app) {
+        await Promise.all([setupProgress(app), setupToasts(app)]);
+    },
+    async beforeRegisterNodeDef(nodeType, nodeData, app) {
+        await uiBeforeRegisterNodeDef(nodeType, nodeData, app);
+    },
+});
